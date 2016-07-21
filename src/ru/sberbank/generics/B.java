6bc6dc1c8edd3d642a3513c6614a05f69d475a06
nmoJavaSchool <nickolai.orlovski@mail.ru>
2016-07-21 20:43:31 +0300

    changes

--- src/ru/sberbank/generics/B.java
+++ src/ru/sberbank/generics/B.java
@@ -3,8 +3,9 @@ package ru.sberbank.generics;
 /**
  * Created by Student on 21.07.2016.
  */
-public class B {
+public class B extends A{
     public static <T> String getType(T obj){
         return obj.getClass().getTypeName();
     }
+
 }
--- src/ru/sberbank/generics/Main.java
+++ src/ru/sberbank/generics/Main.java
@@ -2,6 +2,8 @@ package ru.sberbank.generics;
 import javafx.util.Pair;
 
 import java.util.*;
+import java.util.concurrent.atomic.DoubleAccumulator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -25,10 +27,31 @@ public class Main {
         }.getType());
 
         Integer in = new Integer(10);
-        System.out.println(B.<String>getType());
+        //System.out.println(B.<String>getType(in));
 
         List<Integer> l = new ArrayList();
         l.add(new Integer(10));
         //l.add(new String("1234"));
+
+        A <? extends Integer> aa = new A<>(new Integer(10));//через конструктор можно задать свойство
+        //aa.setField(new Integer(10));//напрямую нельзя
+        Number i = aa.getField();
+        System.out.println(aa.getType());
+
+        A <? extends B> aaa = new A<>(new B());
+        A a1 = aaa.getField();
+        B b1 = aaa.getField();
+        //C c1 = aaa.getField();
+
+        List<? super Number> list = new ArrayList<>();
+        list.add(new Integer(10));
+        list.add(new Double(11));
+        list.add(new Integer(12));
+        list.add(new Double(13));
+
+        list.forEach(System.out::println);
+
+        Object i1 = list.get(0);
+        //Integer ini = list.get(0);
     }
 }
